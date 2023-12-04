65c8ec13e853da7b8a2ab2352783e721167faa0d
Momoyaan <89137647+Momoyaan@users.noreply.github.com>
2023-12-04 16:59:19 +0800

    Add files via upload

---
+++ .config/hypr/scripts/autostart-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+sleep 5
+
+firefox &&
+
+armcord &&
+
+caprine --enable-features=UseOzonePlatform --ozone-platform=wayland &&
+
+teams-for-linux --enable-features=UseOzonePlatform --ozone-platform=wayland 
